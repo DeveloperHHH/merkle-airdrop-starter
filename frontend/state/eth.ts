e129c480f2081a52b9eb72b3e36301c00d621888
Sam Sends <sam@glass.xyz>
2022-01-02 22:13:11 -0500

    frontend: alchemy gets mad for some reason

--- frontend/state/eth.ts
+++ frontend/state/eth.ts
@@ -19,9 +19,8 @@ const wallets: (WalletModule | WalletInitOptions)[] = [
   { walletName: "metamask" },
   {
     walletName: "walletConnect",
-    rpc: {
-      [networkId]: process.env.NEXT_PUBLIC_RPC_URL ?? "",
-    },
+    infuraKey: "89781dd4fcbd49dea42ede60013274de",
+
   },
 ];
 
